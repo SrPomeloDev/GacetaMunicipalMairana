0816157e82bf9441c36d9f624a82666e61cd9154
SrPomeloDev <pabloxzad77@gmail.com>
2026-08-02 20:14:02 -0400

    Compatibilidad Android: transpilar CSS Tailwind v4 (oklch/color-mix/@layer) via PostCSS csstools

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,10 @@
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
+    "@csstools/postcss-cascade-layers": {},
+    "@csstools/postcss-oklab-function": {},
+    "@csstools/postcss-color-mix-function": {},
+    "postcss-nesting": {},
   },
 };
 

67cce23aed6d9a02ba15e062fb9e9e2fa6fd3705
SrPomeloDev <pabloxzad77@gmail.com>
2026-09-13 19:15:07 -0400

    fix: hero inicio a tamano intermedio y tarjeta alcalde centrada

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -79,7 +79,7 @@ export default async function HomePage() {
   return (
     <>
       {/* HERO */}
-      <section className="relative overflow-hidden pt-12 pb-20 sm:pt-28 lg:pt-16">
+      <section className="relative overflow-hidden pt-14 pb-20 sm:pt-28 lg:pt-20 lg:pb-24">
         <Image
           src="/images/plaza.jpg"
           alt=""
